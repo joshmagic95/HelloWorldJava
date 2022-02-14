a84dda13c195564abe8dddf61fc0b90fb377fdec
joshmagic95 <joshfarrell88@gmail.com>
2022-02-14 20:29:06 +0800

    Add integer x and assign as 1

--- HelloWorldJava/src/helloworldjava/HelloWorldJava.java
+++ HelloWorldJava/src/helloworldjava/HelloWorldJava.java
@@ -4,6 +4,7 @@ public class HelloWorldJava {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello World");
+        int x = 1;
     }
     
 }
