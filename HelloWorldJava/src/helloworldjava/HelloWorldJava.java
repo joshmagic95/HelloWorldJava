333e68f0f5919c6f9e50fe21aaaad54368c0b5e3
joshmagic95 <joshfarrell88@gmail.com>
2022-02-14 19:12:11 +0800

    Initial commit

---
+++ HelloWorldJava/src/helloworldjava/HelloWorldJava.java
@@ -0,0 +1,9 @@
+package helloworldjava;
+public class HelloWorldJava {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World");
+    }
+    
+}
